c5dd0d432d3fe31d66b6a073ed8f0b3d936bc981
Cristian Militaru <cristimilitaru97@gmail.com>
2026-02-28 15:19:12 +0200

    FIx white screen flicker when starting app

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
+import { getCurrentWindow } from "@tauri-apps/api/window";
 import "./index.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from "./App.tsx";
 
 render(() => <App />, document.getElementById("root")!);
+requestAnimationFrame(() => {
+  void getCurrentWindow().show();
+});
